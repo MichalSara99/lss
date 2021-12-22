f9fe0c2214ac5e5ac0ef02fad35d7c43a3bc741d
Michal Sara <michal.sara99@gmail.com>
2021-12-22 21:59:22 +0100

    CHANGEs: 1) breaking change in the interface for heat_initial_data_config_2d builder

--- include/lss/pde/configs/heat_initial_data_config_2d.cpp
+++ include/lss/pde/configs/heat_initial_data_config_2d.cpp
@@ -7,10 +7,10 @@ heat_initial_data_config_2d_builder::heat_initial_data_config_2d_builder()
 {
 }
 
-heat_initial_data_config_2d_builder &heat_initial_data_config_2d_builder::initial_condition(
-    std::function<double(double, double)> const &initial_condition)
+heat_initial_data_config_2d_builder &heat_initial_data_config_2d_builder::condition(
+    std::function<double(double, double)> const &condition)
 {
-    initial_condition_ = initial_condition;
+    initial_condition_ = condition;
     return *this;
 }
 
